e6315c4720ba8319b8fb980b8a49897b099a28f2
skaffa <59300989+skaffa@users.noreply.github.com>
2024-07-10 13:25:17 +0200

    Update get_webp.py

--- get_webp.py
+++ get_webp.py
@@ -1,6 +1,6 @@
 from webptools import cwebp
 
 def get_webp(_path):
-    path = path.replace('.png', '.webp')
+    path = _path.replace('.png', '.webp')
     cwebp(_path, path, "-q 95")
     return path
